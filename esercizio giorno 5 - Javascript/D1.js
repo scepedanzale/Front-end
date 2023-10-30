ab73fd58613cc2ad427a204fb6857721f48791e9
Celeste Artuso <celesteartuso@hotmail.com>
2023-10-30 18:15:02 +0100

    test modifica da VSC

--- esercizio giorno 5 - Javascript/D1.js
+++ esercizio giorno 5 - Javascript/D1.js
@@ -24,50 +24,52 @@ REGOLE
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/*
+
   let name = 'Celeste';
-*/
+
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
 */
 
-/*
+
   let num1 = 12;
   let num2 = 20;
   let somma = num1 + num2;
-  console.log(somma);
-*/
+  console.log(num1 + ' + ' + num2 + ' = ' + somma);
+
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
- /*
+ 
   let x = 12;
-*/
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
   Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
 */
 
-/*
+
   name = 'Artuso';
   console.log(name);
   
+/*
   const lettera = 'a';
   lettera = 'b';
+  console.log(lettera);
 */
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
-/*
+
   let num3 = 4;
   console.log(x - num3);
-*/
+
 
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
@@ -75,7 +77,7 @@ REGOLE
  EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
 */
 
-/*
+
   let name1 = 'john';
   let name2 = 'John';
   let control = (name1 != name2);
@@ -84,4 +86,3 @@ REGOLE
   let lower = name2.toLowerCase();
   let newControl = (name1 === lower);
   console.log(newControl);
-*/
